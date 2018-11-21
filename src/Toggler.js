352c7ca784593a627aff2b71202347d1b22cfd83
Kevin Ghadyani <Saturn2888@gmail.com>
2018-11-21 13:25:19 -0600

    Added Toggle component for toggling notification state

--- src/Toggler.js
+++ src/Toggler.js
@@ -2,8 +2,27 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const propTypes = {
-	children: PropTypes.func.isRequired,
+	enabled: PropTypes.bool.isRequired,
+	label: PropTypes.string.isRequired,
+	onClick: PropTypes.func.isRequired,
 }
 
-class Toggler extends Component {
-}
+const Toggler = ({
+	enabled,
+	label,
+	onClick,
+}) => (
+	<span onClick={onClick}>
+		{label}:
+		{' '}
+		{
+			JSON
+			.stringify(enabled)
+		}
+	</span>
+)
+
+Toggler
+.propTypes = propTypes
+
+export default Toggler
