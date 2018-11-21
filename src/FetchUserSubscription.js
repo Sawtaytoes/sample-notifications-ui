e157f6b44b1c5ee30fa6d6fa7400b42216a8edb3
Kevin Ghadyani <Saturn2888@gmail.com>
2018-11-21 13:25:34 -0600

    Added ability to modify the subscription state via the API

--- src/FetchUserSubscription.js
+++ src/FetchUserSubscription.js
@@ -14,7 +14,7 @@ class FetchUserSubscription extends Component {
 		this.fetchData()
 	}
 
-	fetchData() {
+	fetchData = () => {
 		const {
 			subscriptionId,
 			userId,
@@ -48,13 +48,70 @@ class FetchUserSubscription extends Component {
 		))
 	}
 
+	setSubscription = () => {
+		const {
+			subscriptionId,
+			userId,
+		} = this.props
+
+		const { hasSubscription } = this.state
+
+		const promise = (
+			hasSubscription
+			? (
+				fetch(
+					(
+						'http://localhost:3001'
+						.concat('/users')
+						.concat('/subscriptions')
+						.concat(`/${subscriptionId}`)
+					),
+					{
+						headers: {
+							'user-id': userId,
+						},
+						method: 'DELETE',
+					}
+				)
+			)
+			: (
+				fetch(
+					(
+						'http://localhost:3001'
+						.concat('/users')
+						.concat('/subscriptions')
+					),
+					{
+						body: (
+							JSON
+							.stringify({
+								subscriptionId,
+							})
+						),
+						headers: {
+							'Content-Type': 'application/json',
+							'user-id': userId,
+						},
+						method: 'POST',
+					}
+				)
+			)
+		)
+
+		promise
+		.then(this.fetchData)
+	}
+
 	render() {
 		const { children } = this.props
 
 		return (
 			createElement(
 				children,
-				this.state,
+				{
+					...this.state,
+					setSubscription: this.setSubscription,
+				}
 			)
 		)
 	}
